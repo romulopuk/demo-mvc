093770fe420d34cb46c287c35c648aa690f1c273
Romulo Motta <pukpen@gmail.com>
2021-02-21 19:56:08 -0300

    Secao 3 - Classes de dominio

---
+++ src/main/java/com/romulomotta/curso/demomvc/DemoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.romulomotta.curso.demomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcApplication.class, args);
+	}
+
+}
